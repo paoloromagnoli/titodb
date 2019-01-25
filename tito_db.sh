9908ac920a57039b5d311f15e450128604ad891b
Paolo Romagnoli <romagnoli.paolo@gmail.com>
2019-01-25 17:03:17 +0000

    Update tito_db.sh

--- tito_db.sh
+++ tito_db.sh
@@ -50,6 +50,8 @@ echo "STARTUP SCRIPT mysql -u ....."
 
 #	OLD NOOK - mysql --user=$init_db_username --password=$init_db_password < $db_name.sql    # OK
 
+echo -e "Doing this now:"
+echo "mysql -u$init_db_username -h$init_db < $db_name.sql"
 mysql -u$init_db_username -h$init_db < $db_name.sql
 
 echo
